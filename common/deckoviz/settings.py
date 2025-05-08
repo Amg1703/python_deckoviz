ae34b072d27f43f66b5d4995c6a93ea5c4dfb0b3
dharmendra83 <dharmendramehta83@gmail.com>
2025-05-08 20:58:59 +0545

    postgres add

--- common/deckoviz/settings.py
+++ common/deckoviz/settings.py
@@ -99,10 +99,10 @@ WSGI_APPLICATION = 'deckoviz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'NAME': config('DB_NAME', default='postgres'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='postgres'),
+        'HOST': config('DB_HOST', default='postgres'),
         'PORT': config('DB_PORT', default='5432'), 
     }
 } 
